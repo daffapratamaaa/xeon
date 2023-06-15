f061bd20f205038a4c7d4d9fc6a421434b5c3ab7
daffapratamaaa <dp3753158@gmail.com>
2023-06-16 06:26:16 +0700

    first commit

---
+++ settings.js
@@ -0,0 +1,71 @@
+const chalk = require("chalk")
+const fs = require("fs")
+
+//documents variants
+global.doc1 = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
+global.doc2 = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+global.doc3 = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+global.doc4 = 'application/zip'
+global.doc5 = 'application/pdf'
+global.doc6 = 'application/vnd.android.package-archive'
+
+//owmner v card
+global.owner = ['6281236167286'] //ur owner number
+global.ownernomer = "6281236167286" //ur owner number2
+global.ownername = "FallZx" //ur owner name
+global.ytname = "YT: FallXD425" //ur yt chanel name
+global.socialm = "GitHub: FallEzz" //ur github or insta name
+global.location = "India, Mizoram, Aizawl" //ur location
+
+//new
+global.botname = "Fxyz Botz - MD"
+global.ownernumber = '6281236167286'
+global.ownername = '6281236167286'
+global.ownerNumber = ["6281236167286@s.whatsapp.net"]
+global.ownerweb = "https://youtube.com/@4KSanzz"
+global.websitex = "https://linktr.ee/fxyz_"
+global.wagc = "https://chat.whatsapp.com/CfxAwJYP6Rl2J4uOwgavBh"
+global.themeemoji = '🥀'
+global.wm = "Fxyz"
+global.botscript = 'Silahkan Datang Ke Channel 4K Sanzz' //script link
+global.packname = "Ig-fhl.dx17"
+global.author = "4K Sanzz"
+global.creator = "6281236167286@s.whatsapp.net"
+global.prefa = ['','!','.','#','&']
+global.hituet = 0
+
+//media target
+global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
+global.log0 = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur logo pic
+global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
+global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
+
+//menu image maker
+global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+
+//messages
+global.mess = {
+    success: 'Here you go!',
+    admin: 'This feature could be used by admins only!',
+    botAdmin: 'Bot Must Be Admin First!',
+    premime: 'Premium Special Features If You Want to Register Type Rent',
+    owner: 'This feature could be used by owner only',
+    group: 'Features Used Only For Groups!',
+    private: 'Features Used Only For Private Chat!',
+    bot: 'This feature could be used by bot only',
+    wait: 'In process...',
+    linkm: 'Where is the link?',
+    endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
+    nsfw: 'The nsfw feature has not been activated, please contact the admin to activate',
+}
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
